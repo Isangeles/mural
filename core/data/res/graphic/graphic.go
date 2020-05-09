9d3ee01ac2c7fa7c02ca0843c3a3b1ac2e5a5f82
Isangeles <dev@isangeles.pl>
2020-05-09 14:22:01 +0200

    res: packages for graphic and audio data

---
+++ core/data/res/graphic/graphic.go
@@ -0,0 +1,50 @@
+/*
+ * graphic.go
+ *
+ * Copyright 2020 Dariusz Sikora <dev@isangeles.pl>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ *
+ */
+
+// Package with pre-loaded game graphics.
+package graphic
+
+import (
+	"github.com/golang/freetype/truetype"
+
+	"github.com/faiface/pixel"
+)
+
+var (
+	Textures           map[string]pixel.Picture
+	AvatarSpritesheets map[string]pixel.Picture
+	ObjectSpritesheets map[string]pixel.Picture
+	Icons              map[string]pixel.Picture
+	Portraits          map[string]pixel.Picture
+	Fonts              map[string]*truetype.Font
+)
+
+// On init.
+func init() {
+	Textures = make(map[string]pixel.Picture)
+	AvatarSpritesheets = make(map[string]pixel.Picture)
+	ObjectSpritesheets = make(map[string]pixel.Picture)
+	Icons = make(map[string]pixel.Picture)
+	Portraits = make(map[string]pixel.Picture)
+	Fonts = make(map[string]*truetype.Font)
+}

e06d4220b58e51b54508970b978b73eebc3fc2f7
Isangeles <dev@isangeles.pl>
2021-03-29 17:34:00 +0200

    using flame name and version variables from flame package

--- mural.go
+++ mural.go
@@ -1,7 +1,7 @@
 /*
  * mural.go
  *
- * Copyright 2018-2020 Dariusz Sikora <dev@isangeles.pl>
+ * Copyright 2018-2021 Dariusz Sikora <dev@isangeles.pl>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -36,7 +36,6 @@ import (
 	"github.com/faiface/pixel/pixelgl"
 
 	"github.com/isangeles/flame"
-	flameconf "github.com/isangeles/flame/config"
 	flamedata "github.com/isangeles/flame/data"
 	flameres "github.com/isangeles/flame/data/res"
 	"github.com/isangeles/flame/data/res/lang"
@@ -159,7 +158,7 @@ func run() {
 	fpsInfo.Align(mtk.AlignRight)
 	verInfo := mtk.NewText(textParams)
 	verInfo.SetText(fmt.Sprintf("%s(%s)@%s(%s)", config.Name, config.Version,
-		flameconf.Name, flameconf.Version))
+		flame.Name, flame.Version))
 	verInfo.Align(mtk.AlignRight)
 	// Main loop.
 	for !win.Closed() {
